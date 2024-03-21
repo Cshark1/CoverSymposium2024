e7f0b12109fdacec7025afac242c5da06cd874dc
Racoveanu Stefan <racoveanu.stefan@cshark.dev>
2024-03-22 00:06:52 +0100

    Partially fixed Sponsors display on Sponsors page

--- resources/views/components/sponsors-extended.blade.php
+++ resources/views/components/sponsors-extended.blade.php
@@ -11,11 +11,16 @@
 <div class="separator"></div>
 
 <div
-    class="container mx-auto flex w-full flex-wrap items-start justify-center gap-16"
+    class="container mx-auto flex w-full flex-wrap flex-column items-start justify-center gap-16"
 >
     @foreach ($sponsorTiers as $sponsorTier)
-        @foreach ($sponsorTier->sponsors as $sponsor)
-            <x-sponsor-extended :sponsor="$sponsor"></x-sponsor-extended>
-        @endforeach
+        <div class="flex flex-col items-center justify-items-center gap-4 flex-wrap">
+            <p class="h1-header">{{ $sponsorTier->Tier }}</p>
+            <div class="flex items-center">
+                @foreach ($sponsorTier->sponsors as $sponsor)
+                    <x-sponsor-extended :sponsor="$sponsor"></x-sponsor-extended>
+                @endforeach
+            </div>
+        </div>
     @endforeach
 </div>
