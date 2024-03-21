1903b9f5f0ef85c00053309e00fe70d99b3ce731
Racoveanu Stefan <racoveanu.stefan@cshark.dev>
2024-03-21 23:07:12 +0100

    Removed call to Factory from DB migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,8 +21,6 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-        User::factory()->count(10)->make();
     }
 
     /**
